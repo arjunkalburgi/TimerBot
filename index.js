e06a174e04464ea80a93289dd887be3a4ebd4691
Arjun Kalburgi <askalburgi@gmail.com>
2016-05-24 10:59:29 -0600

    index.js fix

--- index.js
+++ index.js
@@ -31,37 +31,39 @@ app.listen(app.get('port'), function() {
 
 // API Endpoint 
 app.post('/webhook/', function (req, res) {
-	messaging_events = req.body.entry[0].messaging
-	for (var i = 0; i < messaging_events.length; i++) {
-		event = req.body.entry[0].messaging[i]
-		sender = event.sender.id
-		if (event.message && event.message.text) {
-			text = event.message.text
-			sendTextMessage(sender, "Text received, echo: " + text.substring(0,200))
-		}
-	}
-	res.sendStatus(200)
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
 })
 
-token = "Ac5i12UGlgBAOH2StfWdIvn3It67OweDYGyqoZBpgqZBTPeTJFvmJJDDY9fAc5t6ENs5DjA8GXSOLMToio7asX3ZAeI3kzPmSTUkClf7BLx2glXpZBHWvW9liJMBQ3lj5eWDwOHHb8p2DpCq6h6QhifnTh8cIR2lRT0g97CFAZDZD"
+var token = "Ac5i12UGlgBAOH2StfWdIvn3It67OweDYGyqoZBpgqZBTPeTJFvmJJDDY9fAc5t6ENs5DjA8GXSOLMToio7asX3ZAeI3kzPmSTUkClf7BLx2glXpZBHWvW9liJMBQ3lj5eWDwOHHb8p2DpCq6h6QhifnTh8cIR2lRT0g97CFAZDZD"
 
 function sendTextMessage(sender, text) {
-	messageData = {text: text}
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages', 
-		qs: {access_token: token}, 
-		method: 'POST', 
-		json: {
-			recipient: {id:sender}, 
-			message: messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log("Error sending messages: ", error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
-		}
-	})
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 }
 
 
